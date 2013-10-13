dfd7a66123d40d89bbb36f0377b2636fca259683
gwynp <gwyn@gwynprice.com>
2013-10-13 10:43:08 -0400

    changed jobs.sh

--- jobs.sh
+++ jobs.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-result=`top -n1 -b| grep Tasks|cut -d : -f 2|cut -d' ' -f 2|tr -d ' '`
-echo "servers.gwyn01.jobs:$result|g" | nc -w 1 -u monitoring 8125
+# Gwyn
+graphitestring=servers.`hostname`.jobs
+monitoringhost=monitoring
 
+result=`top -n1 -b| grep Tasks|cut -d : -f 2|cut -d' ' -f 2|tr -d ' '`
+echo "$graphitestring:$result|g" | nc -w 1 -u $monitoringhost 8125
+exit 0
